7082690e9654da59bf16681f9a6ae176b4dff4ee
Pablo Sabater <pabloosabaterr@gmail.com>
2020-10-13 13:56:52 +0200

    created signip

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,7 +3,23 @@ import jwt from "jsonwebtoken";
 import Role from "../models/Role";
 
 export const singIn = async (req, res) => {
-  res.json("hiu");
+  const userFound  = await User.findOne({email: req.body.email}).populate('roles')
+
+  if(!userFound){
+    res.status(404).json({message: 'user not found'})
+  }
+
+  const matchPassword = await User.comparePassword(req.body.password, userFound.password)
+
+  if(!matchPassword)return res.status(401).json({token: null, message: "invalid password"})
+
+  console.log(userFound)
+
+  const token = jwt.sign({id: userFound._id}, process.env.SECRET, {
+    expiresIn: 86400
+  })
+
+  res.json({token: token});
 };
 export const singUp = async (req, res) => {
   // const userFound = User.find({email})
